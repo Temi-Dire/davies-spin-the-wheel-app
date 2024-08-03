dd544579f4609f7551d0b64560e4e6c3cccc41b5
Temidire <temidireowoeye@gmail.com>
2024-08-03 13:41:36 +0100

    fix: endpoint

--- src/hooks/useSendData.ts
+++ src/hooks/useSendData.ts
@@ -5,7 +5,7 @@ type ElementData = any;
 
 export const useSendData = (user_id: string, bot_id: string) => {
 
-    const apiClient = new ApiClient<ElementData>(`api/${bot_id}/${user_id}`);
+    const apiClient = new ApiClient<ElementData>(`api/${bot_id}/${user_id}/spin`);
 
     return useMutation({
         mutationFn: (s: any) => apiClient.post(s),
